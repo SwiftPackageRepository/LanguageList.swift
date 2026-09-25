5423b19616bfcc015e73a2459227bbf37c904951
Sascha Müllner <sascha.muellner@gmail.com>
2026-09-25 17:51:23 +0200

    Update SwiftUIPlus dependency to 0.3.2

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/Webblazer/SwiftUIPlus.swift.git",
-            exact: "0.3.1"
+            exact: "0.3.2"
         ),
     ],
     targets: [
